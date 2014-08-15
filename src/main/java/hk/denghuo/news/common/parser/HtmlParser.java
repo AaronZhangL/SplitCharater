2f80e640bc8687c6afb74a6243eac38adaefccd9
AaronZhangL <aaronzhang0@gmail.com>
2014-08-15 15:12:53 +0900

    Updated xerces1 to xerces2 and added tools.

---
+++ src/main/java/hk/denghuo/news/common/parser/HtmlParser.java
@@ -0,0 +1,20 @@
+package main.java.hk.denghuo.news.common.parser;
+
+/*import org.apache.xerces.dom.*;
+import org.apache.xerces.parsers.DOMParser;*/
+import org.w3c.dom.Document;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.SAXNotRecognizedException;
+import org.xml.sax.SAXNotSupportedException;
+
+public class HtmlParser {
+
+	public static void main(String[] args) {
+
+		//DOMParser domParser = new DOMParser();
+
+	}
+
+}
